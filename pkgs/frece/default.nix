04b1d29bf4a1199931e84984c4d6aa05214554a8
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-10-23 22:04:02 -0700

    fix: frece sha256 sums

--- pkgs/frece/default.nix
+++ pkgs/frece/default.nix
@@ -8,10 +8,10 @@ rustPlatform.buildRustPackage rec {
     owner = "YodaEmbedding";
     repo = pname;
     rev = "v${version}";
-    sha256 = lib.fakeSha256;
+    sha256 = "1p1pxf730jcr59n3fa1n7cqbqiv5wynm8iazdpm3mfsa7slqh208";
   };
 
-  cargoSha256 = lib.fakeSha256;
+  cargoSha256 = "0i89hiji1b9x830shc4slv56s5xdi89xs9shbba4p5zsnbbpvcvq";
 
   meta = with lib; {
     description = "Frecency sorted database";
