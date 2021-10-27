49c2308de80f48711946d318e0ebf4a4955e7788
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-10-27 10:30:43 -0700

    style: deindent

--- configuration.nix
+++ configuration.nix
@@ -108,43 +108,43 @@
 
     libinput.enable = true;
     videoDrivers = [ "nvidia" ];
-
-    config = ''
-      Section "InputClass"
-        Identifier "My Mouse"
-        Driver "libinput"
-        MatchIsPointer "on"
-        MatchProduct "Logitech MX Vertical"
-        MatchVendor "Logitech"
-        Option "AccelProfile" "flat"
-        Option "AccelSpeed" "-0.75"
-        # Option "AccelerationNumerator" "1"
-        # Option "AccelerationDenominator" "1"
-        # Option "AccelerationThreshold" "0"
-      EndSection
-
-      Section "Device"
-          Identifier "Device0"
-          Driver     "nvidia"
-          VendorName "NVIDIA Corporation"
-          BoardName  "GeForce GTX 1060 6GB"
-	  # FIX I2C ERRORS
-          # http://www.ddcutil.com/nvidia/
-          # https://devtalk.nvidia.com/default/topic/572292/-solved-does-gddccontrol-work-for-anyone-here-nvidia-i2c-monitor-display-ddc/#4309293
-          Option "RegistryDwords" "RMUseSwI2c=0x01; RMI2cSpeed=100"
-	  # SCREEN TEARING
-          # https://bbs.archlinux.org/viewtopic.php?pid=1716977#p1716977
-          # Option "TripleBuffer" "True"
-          # Option "AllowIndirectGLXProtocol" "off"
-          # "ForceFullComposition" can make gaming low fps? Though this doesn't have "Full" in it...
-          # TODO shouldn't that be DVI-D-0 not I-1? Try it out.
-          # Option "metamodes" "nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
-          # Option "metamodes" "DVI-D-0: nvidia-auto-select {ForceCompositionPipeline=On}"
-          # Option "metamodes" "DVI-I-1: nvidia-auto-select {ForceCompositionPipeline=On}"
-          # Option "metamodes" "nvidia-auto-select @1920x1080 +0+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0, ForceFullCompositionPipeline=On}"
-      EndSection
-    '';
   };
+
+  services.xserver.config = ''
+    Section "InputClass"
+      Identifier "My Mouse"
+      Driver "libinput"
+      MatchIsPointer "on"
+      MatchProduct "Logitech MX Vertical"
+      MatchVendor "Logitech"
+      Option "AccelProfile" "flat"
+      Option "AccelSpeed" "-0.75"
+      # Option "AccelerationNumerator" "1"
+      # Option "AccelerationDenominator" "1"
+      # Option "AccelerationThreshold" "0"
+    EndSection
+
+    Section "Device"
+      Identifier "Device0"
+      Driver     "nvidia"
+      VendorName "NVIDIA Corporation"
+      BoardName  "GeForce GTX 1060 6GB"
+      # FIX I2C ERRORS
+      # http://www.ddcutil.com/nvidia/
+      # https://devtalk.nvidia.com/default/topic/572292/-solved-does-gddccontrol-work-for-anyone-here-nvidia-i2c-monitor-display-ddc/#4309293
+      Option "RegistryDwords" "RMUseSwI2c=0x01; RMI2cSpeed=100"
+      # SCREEN TEARING
+      # https://bbs.archlinux.org/viewtopic.php?pid=1716977#p1716977
+      # Option "TripleBuffer" "True"
+      # Option "AllowIndirectGLXProtocol" "off"
+      # "ForceFullComposition" can make gaming low fps? Though this doesn't have "Full" in it...
+      # TODO shouldn't that be DVI-D-0 not I-1? Try it out.
+      # Option "metamodes" "nvidia-auto-select +0+0 {ForceCompositionPipeline=On}"
+      # Option "metamodes" "DVI-D-0: nvidia-auto-select {ForceCompositionPipeline=On}"
+      # Option "metamodes" "DVI-I-1: nvidia-auto-select {ForceCompositionPipeline=On}"
+      # Option "metamodes" "nvidia-auto-select @1920x1080 +0+0 {ViewPortIn=1920x1080, ViewPortOut=1920x1080+0+0, ForceFullCompositionPipeline=On}"
+    EndSection
+  '';
 
   services.localtime.enable = true;
 
@@ -245,61 +245,59 @@
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.cudaSupport = true;
 
-  nixpkgs.config = {
-    packageOverrides = pkgs: rec {
+  nixpkgs.config.packageOverrides = pkgs: rec {
 
-      # neovim = pkgs.neovim.override (
-      #   pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins:
-      #     pkgs.tree-sitter.allGrammars)
-      # );
+    # neovim = pkgs.neovim.override (
+    #   pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins:
+    #     pkgs.tree-sitter.allGrammars)
+    # );
 
-      # opencv = pkgs.opencv.override (old: {
-      #   enableEigen = true;
-      #   enableFfmpeg = true;
-      #   enableGStreamer = true;
-      #   enableGtk2 = true;
-      #   enableJPEG = true;
-      #   enablePNG = true;
-      #   enablePython = true;
-      #   enableTIFF = true;
-      #   # enableJPEG2K = true;
-      # });
+    # opencv = pkgs.opencv.override (old: {
+    #   enableEigen = true;
+    #   enableFfmpeg = true;
+    #   enableGStreamer = true;
+    #   enableGtk2 = true;
+    #   enableJPEG = true;
+    #   enablePNG = true;
+    #   enablePython = true;
+    #   enableTIFF = true;
+    #   # enableJPEG2K = true;
+    # });
 
-      # opencv4 = pkgs.opencv4.override (old: {
-      #   enableContrib = true;
-      #   enableEigen = true;
-      #   enableJPEG = true;
-      #   enableOpenblas = true;
-      #   enablePNG = true;
-      #   enableTIFF = true;
-      #   enableWebP = true;
+    # opencv4 = pkgs.opencv4.override (old: {
+    #   enableContrib = true;
+    #   enableEigen = true;
+    #   enableJPEG = true;
+    #   enableOpenblas = true;
+    #   enablePNG = true;
+    #   enableTIFF = true;
+    #   enableWebP = true;
 
-      #   enableFfmpeg = true;
-      #   enableGPhoto2 = true;
-      #   enableGStreamer = true;
-      #   enableTbb = true;
-      #   # enableTesseract = true;
+    #   enableFfmpeg = true;
+    #   enableGPhoto2 = true;
+    #   enableGStreamer = true;
+    #   enableTbb = true;
+    #   # enableTesseract = true;
 
-      #   enableGtk2 = true;
-      #   enableGtk3 = true;
-      #   enableIpp = true;
-      #   enablePython = true;
-      #   enableUnfree = true;
-      # });
+    #   enableGtk2 = true;
+    #   enableGtk3 = true;
+    #   enableIpp = true;
+    #   enablePython = true;
+    #   enableUnfree = true;
+    # });
 
-      polybar = (pkgs.polybar.override {
-        i3Support = true;
-      });
+    polybar = (pkgs.polybar.override {
+      i3Support = true;
+    });
 
-      # thunar = (pkgs.xfce.thunar.override {
-      #   thunarPlugins = [
-      #     pkgs.xfce.thunar-archive-plugin
-      #     pkgs.xfce.thunar-dropbox-plugin
-      #     pkgs.xfce.thunar-volman
-      #   ];
-      # });
+    # thunar = (pkgs.xfce.thunar.override {
+    #   thunarPlugins = [
+    #     pkgs.xfce.thunar-archive-plugin
+    #     pkgs.xfce.thunar-dropbox-plugin
+    #     pkgs.xfce.thunar-volman
+    #   ];
+    # });
 
-    };
   };
 
   environment.variables = rec {
