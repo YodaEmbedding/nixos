d8b2842d6c3ac93d4d81fedca0addfdb001706d2
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-10-31 01:32:38 -0700

    refactor: extract xdg

--- configuration.nix
+++ configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     ./hardware-configuration.nix
     ./hosts/pc_mordor
+    ./xdg
   ];
 
   # Use the systemd-boot EFI boot loader.
@@ -213,26 +214,6 @@
     TERMINAL = "alacritty";
     VISUAL = "nvim";
   };
-
-  environment.etc."xdg/gtk-3.0/settings.ini".text = ''
-    [Settings]
-    gtk-font-name=Noto Sans Regular 10
-    gtk-theme-name=Adapta-Nokto-Eta
-    gtk-icon-theme-name=breeze-dark
-    gtk-cursor-theme-name=Breeze_Snow
-    gtk-cursor-theme-size=0
-    gtk-application-prefer-dark-theme=1
-    gtk-toolbar-style=GTK_TOOLBAR_ICONS
-    gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
-    gtk-button-images=1
-    gtk-menu-images=1
-    gtk-enable-event-sounds=1
-    gtk-enable-input-feedback-sounds=1
-    gtk-xft-antialias=1
-    gtk-xft-hinting=1
-    gtk-xft-hintstyle=hintfull
-    gtk-xft-rgba=rgb
-  '';
 
   environment.systemPackages = (import ./packages.nix {
     pkgs = pkgs;
---
+++ xdg/gtk.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  environment.etc."xdg/gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-font-name=Noto Sans Regular 10
+    gtk-theme-name=Adapta-Nokto-Eta
+    gtk-icon-theme-name=breeze-dark
+    gtk-cursor-theme-name=Breeze_Snow
+    gtk-cursor-theme-size=0
+    gtk-application-prefer-dark-theme=1
+    gtk-toolbar-style=GTK_TOOLBAR_ICONS
+    gtk-toolbar-icon-size=GTK_ICON_SIZE_LARGE_TOOLBAR
+    gtk-button-images=1
+    gtk-menu-images=1
+    gtk-enable-event-sounds=1
+    gtk-enable-input-feedback-sounds=1
+    gtk-xft-antialias=1
+    gtk-xft-hinting=1
+    gtk-xft-hintstyle=hintfull
+    gtk-xft-rgba=rgb
+  '';
+}
