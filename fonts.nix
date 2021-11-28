dd9c9ad55f0fe0078d8d422c5231759194f9bb9d
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-11-28 00:48:08 -0800

    style: remove auto-generated comments for font config and i18n

--- fonts.nix
+++ fonts.nix
@@ -5,24 +5,6 @@
 }:
 
 {
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-  # console = {
-  #   font = "Lat2-Terminus16";
-  #   keyMap = "us";
-  # };
-
-  # fonts = {
-  #   fontDir.enable = true;
-  #   fonts = with pkgs; [
-  #     ubuntu_font_family
-  #   ];
-  #   fontconfig.defaultFonts = {
-  #     sansSerif = [ "Ubuntu" ];
-  #     monospace = [ "Ubuntu Mono" ];
-  #   };
-  # };
-
   fonts.fonts = with pkgs; [
     fira-code
     fira-code-symbols
