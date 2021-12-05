e22828d2183588e0dad90175f62667d18c3a50c3
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-12-04 17:54:53 -0800

    feat: nix flakes init

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
+    # nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+    # nur.url = "github:nix-community/NUR";
+    # home-manager.url = "github:nix-community/home-manager/release-21.11";
+    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.PC-Mordor-NixOS = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
