29445d6551a1a643796ecdb1b177cd054542b62c
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-12-05 20:07:06 -0800

    feat: nixpkgs overlay for picom

--- nixpkgs_conf.nix
+++ nixpkgs_conf.nix
@@ -10,7 +10,17 @@
     cudaSupport = true;
   };
 
-  nixpkgs.config.packageOverrides = pkgs: rec {
-
-  };
+  nixpkgs.overlays = [
+    (self: super: {
+      picom = super.picom.overrideAttrs (prev: {
+        version = "git";
+        src = pkgs.fetchFromGitHub {
+          owner = "yshui";
+          repo = "picom";
+          rev = "31e58712ec11b198340ae217d33a73d8ac73b7fe";
+          sha256 = "sha256-VBnIzisg/7Xetd/AWVHlnaWXlxX+wqeYTpstO6+T5cE=";
+        };
+      });
+    })
+  ];
 }
