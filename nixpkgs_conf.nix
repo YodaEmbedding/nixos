5bf26d736fa00789fb881864e9bb8a8f144e3e11
Mateen Ulhaq <mulhaq2005@gmail.com>
2021-12-06 00:22:06 -0800

    feat: override picom with picom-next

--- nixpkgs_conf.nix
+++ nixpkgs_conf.nix
@@ -12,14 +12,8 @@
 
   nixpkgs.overlays = [
     (self: super: {
-      picom = super.picom.overrideAttrs (prev: {
-        version = "git";
-        src = pkgs.fetchFromGitHub {
-          owner = "yshui";
-          repo = "picom";
-          rev = "31e58712ec11b198340ae217d33a73d8ac73b7fe";
-          sha256 = "sha256-VBnIzisg/7Xetd/AWVHlnaWXlxX+wqeYTpstO6+T5cE=";
-        };
+      picom = super.picom.overrideAttrs (oldAttrs: rec {
+        inherit (super.picom-next) pname version src meta;
       });
     })
   ];
