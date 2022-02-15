b9984266b1206fbba5b8d7aa7bb316d7fd61be6b
Mateen Ulhaq <mulhaq2005@gmail.com>
2022-02-14 20:59:35 -0800

    chore: add nixos-fhs-compat flake

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
     nur.url = "github:nix-community/NUR";
     # home-manager.url = "github:nix-community/home-manager/release-21.11";
     # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-fhs-compat.url = "github:balsoft/nixos-fhs-compat";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, nur } @inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, nur, nixos-fhs-compat } @inputs:
   {
     nixosConfigurations.PC-Mordor-NixOS = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
