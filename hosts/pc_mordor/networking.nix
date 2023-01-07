fcd6d711ba07950b5f41c412e4b7ba5efeb3d690
Mateen Ulhaq <mulhaq2005@gmail.com>
2023-01-06 22:54:16 -0800

    hosts(pc_mordor): networking.useDHCP no longer deprecated since NixOS 22.05

--- hosts/pc_mordor/networking.nix
+++ hosts/pc_mordor/networking.nix
@@ -11,12 +11,6 @@
 
     networkmanager.enable = true;
 
-    # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-    # Per-interface useDHCP will be mandatory in the future, so this generated config
-    # replicates the default behaviour.
-    useDHCP = false;
-    interfaces.enp2s0.useDHCP = true;
-
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
